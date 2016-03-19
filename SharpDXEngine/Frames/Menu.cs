c48fef3d9962f64a25da3d39c9cd9aa811cf7646
Giuseppe Menti <mentifg@gmail.com>
2016-03-19 14:33:47 -0300

    FEA(Cursor): Adiciona texture diferente quando botão esquerdo do mouse estiver pressionado

---
+++ SharpDXEngine/Frames/Menu.cs
@@ -0,0 +1,35 @@
+﻿using GameClient.Controllers;
+using Microsoft.Xna.Framework;
+using SharpDXEngine.Frames.Components;
+
+namespace SharpDXEngine.Frames
+{
+    class Menu
+    {
+        private Picture background;
+        private TextBox textBox;
+        
+        public void Initialize()
+        {
+            background = new Picture(new Vector2(0, 0));
+            textBox = new TextBox(); 
+        }
+
+        public void Load()
+        {
+            background.Load();
+            textBox.Load();
+        }
+
+        public void Update()
+        {
+            textBox.Update();
+        }
+
+        public void Draw()
+        {
+            background.Draw();
+            textBox.Draw();
+        }
+    }
+}
