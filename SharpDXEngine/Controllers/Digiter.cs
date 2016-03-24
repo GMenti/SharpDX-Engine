b55b4475926c6f2a01dc53a6c40a01b94669c376
Giuseppe Menti <mentifg@gmail.com>
2016-03-24 16:18:36 -0300

    Não faço ideia

---
+++ SharpDXEngine/Controllers/Digiter.cs
@@ -0,0 +1,38 @@
+﻿using GameClient.Components;
+using Microsoft.Xna.Framework;
+using MonoGame.Extended.Timers;
+using System;
+
+namespace SharpDXEngine.Controllers
+{
+    class Digiter
+    {
+        private string text;
+        private ContinuousClock keyTimer;
+
+        public Digiter()
+        {
+            this.text = "";
+            this.keyTimer = new ContinuousClock(1);
+            this.keyTimer.Start();
+        }
+
+        public string getKeyPressed(GameTime gameTime)
+        {
+            this.updateValue(gameTime);
+            return this.text;
+        }
+
+        private void updateValue(GameTime gameTime)
+        {
+            InputSystem.CharEntered += delegate (Object o, CharacterEventArgs e) {
+                this.keyTimer.Update(gameTime);
+
+                keyTimer.Tick += delegate (object sender, EventArgs e2) {
+                    this.text += e.Character;
+                };
+            };
+        }
+
+    }
+}
---
+++ SharpDXEngine/Controllers/Picture.cs
@@ -0,0 +1,44 @@
+﻿using GameClient;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SharpDXEngine.Controllers
+{
+    class Picture
+    {
+        protected Texture2D texture;
+        public Vector2 position;
+        public float rotation;
+        public Vector2 expandFactor;
+        public Vector2 spriteOrigin;
+
+        public Picture(Vector2 position)
+        {
+            this.position = position;
+            this.rotation = 0f;
+            this.expandFactor = new Vector2(1f, 1f);
+            this.spriteOrigin = Vector2.Zero;
+        }
+
+        public void Load(ContentManager content, string filePath)
+        {
+            this.texture = content.Load<Texture2D>(filePath);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(
+                this.texture,
+                this.position,
+                null,
+                Color.White,
+                this.rotation,
+                this.spriteOrigin,
+                expandFactor,
+                SpriteEffects.None,
+                0f
+            );
+        }
+    }
+}
