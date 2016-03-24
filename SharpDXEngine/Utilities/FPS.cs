e92155015c8a2a820a450553d8d4f6a3411922cb
Giuseppe Menti <mentifg@gmail.com>
2016-03-24 20:31:00 -0300

    Teste

--- SharpDXEngine/Utilities/FPS.cs
+++ SharpDXEngine/Utilities/FPS.cs
@@ -6,32 +6,31 @@ using MonoGame.Extended.Timers;
 using SharpDXEngine.Controllers;
 using System;
 
-namespace GameClient.Utilities
-{
-    class FPS : Label
-    {
+namespace GameClient.Utilities {
+    class FPS : Label {
         private FramesPerSecondCounter fpsCounter;
         private ContinuousClock fpsTimer;
+        private int fps;
 
-        public FPS(string text, Color color, Vector2 position) : base(text, color, position)
-        {
+        public FPS(string text, Color color, Vector2 position) : base(text, color, position) {
             this.fpsCounter = new FramesPerSecondCounter();
             this.fpsTimer = new ContinuousClock(1);
             this.fpsTimer.Start();
         }
 
-        public void Refresh(GameTime gameTime)
-        {
+        public void Refresh(GameTime gameTime) {
             base.Update(gameTime);
             this.fpsCounter.Update(gameTime);
             this.fpsTimer.Update(gameTime);
+            this.fps = (int)fpsCounter.TotalFrames;
 
             fpsTimer.Tick += delegate (object sender, EventArgs e) {
-                base.text = "FPS: " + (int)fpsCounter.CurrentFramesPerSecond;
+                base.text = "FPS: " + this.fps;
+                this.fpsCounter.Reset();
             };
         }
 
 
-        
+
     }
 }
